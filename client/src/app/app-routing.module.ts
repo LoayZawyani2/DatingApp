ac66c08e270c089d27336074b1da84828488a163
Loay Zawyani <loay.zawyani@xngage.com>
2021-01-23 21:10:48 +0200

    Add some componenets and add routing pathes at app- routing.modules and make the nav links directs to them

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListsComponent } from './lists/lists.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemeberListComponent } from './members/memeber-list/memeber-list.component';
+import { MessagesComponent } from './messages/messages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'members', component: MemeberListComponent},
+  { path: 'members/:id', component: MemberDetailComponent},
+  { path: 'lists', component: ListsComponent},
+  { path: 'messages', component: MessagesComponent},
+  
+  // this means if the user enter something not specified here 
+  { path: '**', component: HomeComponent, pathMatch: 'full'}, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,13 +10,21 @@ import { NavComponent } from './nav/nav.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemeberListComponent } from './members/memeber-list/memeber-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemeberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
